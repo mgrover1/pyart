63938a801130b82e958318a583f3fdec6c61053b
mgrover1 <mgrover1@users.noreply.github.com>
2022-03-11 03:09:39 +0000

    deploy: 6c3be4258fd6e58778b11877b368ff51e7084706

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.7.dev+c2ebbb9',
+    VERSION: '1.11.7.dev+6c3be42',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -8,5 +8,5 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: true
 };
